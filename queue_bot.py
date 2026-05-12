117296cd1d3365fd9c9f90cf437a2b3a6cd2e2ef
TwoLettuce <grantharris0323@gmail.com>
2026-05-12 17:01:14 -0600

    added new commands

--- queue_bot.py
+++ queue_bot.py
@@ -4,9 +4,8 @@ import discord
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+load_dotenv("./bot-password.env")
+BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,24 +18,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    #ensure the message is not a message from the bot to avoid infinite loops
     if message.author == client.user:
+        return
+    
+    # ensure the command was sent in the right chat
+    if message.channel.name != "help-queue-chat":
+        if message.content.startswith('$'):
+            await message.channel.send(f'To invoke this bot, send messages in the <#{message.channel.id}>!')
         return
     
     print(f'We have received a message: {message.content}')
 
+    # testing
     if message.content.startswith('$join'):
         await message.channel.send('Hello!')
 
     # join help queue
+    # get the TA role so as to be able to mention it. Not sure if we should keep this or not because it will ping all 
+    # TAs and not just the active ones. I feel like we are pretty good at looking at the help queue chat 
+    # so I lean towards not needing it, but if we need to know how to mention here it is.
     guild = message.guild
     ta_role = discord.utils.get(guild.roles, name="TA")
-    if message.content.startswith('$queue'):
-        #The role ID is what goes inside the hoinkies and after the '@&'
+    if message.content.startswith('$queue'):        
         await message.channel.send(f'You are now in the queue. The next available {ta_role.mention} will help you!')
 
     # assist student
     if message.content.startswith('$help'):
-        if message.role != ta_role:
+        if not message.author.roles.contains(ta_role):
             message.channel.send('Access to this command is denied')
         await message.channel.send('You are now helping a student!')
 
@@ -44,6 +53,10 @@ async def on_message(message):
     if message.content.startswith('$help-passoff'):
         await message.channel.send('You are now helping a student with their passoff!')
 
-    
+    if message.content.startswith('$done'):
+        await message.channel.send('You are now done helping a student!')
 
-client.run(TOKEN)
+    
+print(f'{BOT_TOKEN.__class__}')
+
+client.run(BOT_TOKEN)
