ccec99a3b06e6d4e958695547c325fc2281ff51d
Grant Harris <grantharris0323@gmail.com>
2026-05-26 14:34:54 -0600

    Add remove button, add timeouts to messages sent by bot.

--- modals.py
+++ modals.py
@@ -1,10 +1,10 @@
 import discord
 from db import get_times_helped_today
-
 class HelpModal(discord.ui.Modal, title="Request Help"):
 
     question = discord.ui.TextInput(
         label="What do you need help with?",
+        placeholder="Please enter more than just the phase you are currently working on :)",
         style=discord.TextStyle.paragraph,
         max_length=300
     )
@@ -29,8 +29,8 @@ class HelpModal(discord.ui.Modal, title="Request Help"):
         value = self.in_person.value.lower()
         if value not in ("o", "p"):
             await interaction.response.send_message(
-                "Join Queue _**Failed**_. Please enter `o` or `p` to indicate whether you are online or in-person",
-                ephemeral=True
+                "Join Queue _**Failed**_. Please enter either `o` or `p` to indicate whether you are online or in-person",
+                ephemeral=True,
             )
             return
         
@@ -47,7 +47,7 @@ class HelpModal(discord.ui.Modal, title="Request Help"):
                 await channel.send(
                     f"{interaction.user.display_name} has joined the help queue - {"In-person" if value=="p" else "Online"} - {self.question.value} "
                     f"(helped {times_helped} time{'s' if times_helped != 1 else ''} today)",
-                    delete_after=60*30
+                    delete_after=60*5
                 )
 
 
@@ -75,8 +75,8 @@ class PassoffModal(discord.ui.Modal, title="Request Passoff"):
         for channel in interaction.guild.channels:
             if channel.name == "ta-bot-chat":
                 await channel.send(f"{interaction.user.display_name} has requested a passoff - {"In-person" if value=="p" else "Online"} - {self.phase.value}",
-                                   # disappear after 30 minutes
-                                   delete_after=60*30)
+                                   # disappear after 5 minutes
+                                   delete_after=60*5)
                 
 class ClearConfirmModal(discord.ui.Modal, title="Clear Confirmation"):
     warning = discord.ui.TextDisplay("Are you sure? This will remove all students from the queue, and cannot be undone.")
@@ -87,7 +87,24 @@ class ClearConfirmModal(discord.ui.Modal, title="Clear Confirmation"):
             await interaction.response.send_message("Clear aborted", ephemeral=True, delete_after=30)
         else:
             await interaction.client.queue.clear()
-            await interaction.response.send_message("Queue cleared", delete_after=60*10)
+            await interaction.response.send_message("Queue cleared", delete_after=60*5)
             for channel in interaction.guild.channels:
                 if channel.name == "help-queue-chat":
-                    await channel.send("All students have been removed from the queue. Sorry we couldn't get to you!", delete_after=60*20)
+                    await channel.send("All students have been removed from the queue. Sorry we couldn't get to you!", delete_after=60*10)
+
+
+class RemoveConfirmModal(discord.ui.Modal, title="Removal Confirmation"):
+    warning = discord.ui.TextDisplay("Are you sure? This will remove the student from the queue.")
+    input: discord.ui.TextInput = discord.ui.TextInput(label="Please input student username.", placeholder="bsharplydian", min_length=1)
+    reason: discord.ui.TextInput = discord.ui.TextInput(label="Provide a reason (optional)", placeholder="You've used the queue too many times today", required=False)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        for entry in interaction.client.queue.entries:
+            if entry.username == self.input.value:
+                user: discord.User = await interaction.client.fetch_user(entry.user_id)
+                await interaction.client.queue.remove(user.id)
+                await user.send(f"You have been removed from the CS240 help queue. {"Reason: " if self.reason.value != "" else ""}{self.reason.value}")
+                await interaction.response.send_message(f"{user.display_name} has been removed from the queue by {interaction.user.display_name}.", delete_after=60*5)
+                return
+            
+        await interaction.response.send_message(f"No student with the username \"{self.input.value}\" in the queue.", ephemeral=True, delete_after=20)
--- ui.py
+++ ui.py
@@ -3,48 +3,48 @@ from typing import Optional
 import discord
 from db import increment_help, get_student_info, get_times_helped_today
 from records import QueueEntry
-from modals import HelpModal, PassoffModal, ClearConfirmModal
+from modals import HelpModal, PassoffModal, ClearConfirmModal, RemoveConfirmModal
 
 
 class QueueView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Need Help", style=discord.ButtonStyle.primary, custom_id="need_help")
+    @discord.ui.button(label="Need Help", style=discord.ButtonStyle.primary, custom_id="need_help", emoji="🙏")
     async def help_btn(self, interaction: discord.Interaction, button):
         if not await queue_is_open(interaction):
             return await interaction.response.send_message(
-                "Queue is closed.", ephemeral=True
+                "Queue is closed.", ephemeral=True, delete_after=20
             )
         
         if await already_in_queue(interaction):
             return await interaction.response.send_message(
-                "You are already in the queue.", ephemeral=True
+                "You are already in the queue.", ephemeral=True, delete_after=10
             )
 
         today_help_count = get_times_helped_today(interaction.user.id)
         await interaction.response.send_modal(HelpModal(today_help_count))
 
-    @discord.ui.button(label="Passoff", style=discord.ButtonStyle.success, custom_id="passoff")
+    @discord.ui.button(label="Passoff", style=discord.ButtonStyle.success, custom_id="passoff", emoji="💪")
     async def passoff_btn(self, interaction: discord.Interaction, button):
         if not await queue_is_open(interaction):
             return await interaction.response.send_message(
-                "Queue is closed.", ephemeral=True
+                "Queue is closed.", ephemeral=True, delete_after=20
             )
         if await already_in_queue(interaction):
             return await interaction.response.send_message(
-                "You are already in the queue.", ephemeral=True
+                "You are already in the queue.", ephemeral=True, delete_after=10
             )
         
         await interaction.response.send_modal(PassoffModal())
 
-    @discord.ui.button(label="Leave Queue", style=discord.ButtonStyle.danger, custom_id="leave_queue")
+    @discord.ui.button(label="Leave Queue", style=discord.ButtonStyle.danger, custom_id="leave_queue", emoji="🚪")
     async def leave_btn(self, interaction: discord.Interaction, button):
         if await interaction.client.queue.is_in_queue(interaction.user.id):
             await interaction.client.queue.remove(interaction.user.id)
-            await interaction.response.send_message("Removed from queue.", ephemeral=True)
+            await interaction.response.send_message("Removed from queue.", ephemeral=True, delete_after=20)
         else:
-            await interaction.response.send_message("You aren't currently in the queue", ephemeral=True)
+            await interaction.response.send_message("You aren't currently in the queue", ephemeral=True, delete_after=10)
 
 
 class TAView(discord.ui.View):
@@ -55,11 +55,11 @@ class TAView(discord.ui.View):
     open_msg: str = "The Help Queue is now open!"
     close_msg: str = "The Help Queue is now closed. If you are still on the queue, the TAs will help until their hours are over."
     
-    @discord.ui.button(label="Open Queue", style=discord.ButtonStyle.green, custom_id="open_queue")
-    async def open(self, interaction: discord.Interaction, button):
-        if not interaction.client.queue.is_open:
+    @discord.ui.button(label="Open Queue", style=discord.ButtonStyle.green, custom_id="open_queue", emoji="🔓")
+    async def open(self, interaction: discord.Interaction, button: discord.Button):
+        if not queue_is_open(interaction):
             interaction.client.queue.is_open = True
-            await interaction.response.send_message("Queue opened.", ephemeral=True, delete_after=60*60*12)
+            await interaction.response.send_message("Queue opened.", ephemeral=True, delete_after=60*5)
             help_channel = get_channel(interaction, "help-queue-chat")
 
             if help_channel.last_message is not None and help_channel.last_message.content == self.close_msg:
@@ -67,42 +67,43 @@ class TAView(discord.ui.View):
             await help_channel.send(self.open_msg, delete_after=60*60*12)
             return
         else:
-            await interaction.response.send_message("Queue is already open!", ephemeral=True)
+            await interaction.response.send_message("Queue is already open!", ephemeral=True, delete_after=10)
 
-    @discord.ui.button(label="Close Queue", style=discord.ButtonStyle.red, custom_id="close_queue")
+    @discord.ui.button(label="Close Queue", style=discord.ButtonStyle.red, custom_id="close_queue", emoji="🔏")
     async def close(self, interaction: discord.Interaction, button):
-        if interaction.client.queue.is_open:
+        if queue_is_open(interaction):
             interaction.client.queue.is_open = False
-            await interaction.response.send_message("Queue closed.", ephemeral=True)
+            await interaction.response.send_message("Queue closed.", ephemeral=True, delete_after=60*5)
             help_channel = get_channel(interaction, "help-queue-chat")
             if help_channel.last_message is not None and help_channel.last_message.content == self.open_msg:
                 await help_channel.last_message.delete()
             await help_channel.send(self.close_msg, delete_after=60*60*12)
             return
         else:
-            await interaction.response.send_message("Queue is already closed!", ephemeral=True)
+            await interaction.response.send_message("Queue is already closed!", ephemeral=True, delete_after=10)
 
-    @discord.ui.button(label="View Queue", style=discord.ButtonStyle.secondary, custom_id="view_queue")
+    @discord.ui.button(label="View Queue", style=discord.ButtonStyle.secondary, custom_id="view_queue", emoji="👀")
     async def view_btn(self, interaction: discord.Interaction, button):
         text = await interaction.client.queue.view()
         await interaction.response.send_message(text, ephemeral=True)
 
 
-    @discord.ui.button(label="Next", style=discord.ButtonStyle.blurple, custom_id="next")
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.blurple, custom_id="next", emoji="➡️")
     async def next(self, interaction: discord.Interaction, button):
         entry: Optional[QueueEntry] = await interaction.client.queue.next()
 
         if not entry:
-            return await interaction.response.send_message("Queue is empty.", ephemeral=True)
+            return await interaction.response.send_message("Queue is empty.", ephemeral=True, delete_after=10)
 
         if not entry.is_passoff:
             increment_help(entry.user_id, entry.username)
 
-        await notify_tas(interaction, f"{interaction.user.display_name} is now helping {entry.username}")
+        await notify_tas(interaction, f"{interaction.user.display_name} is now helping {entry.username}", delete_after=60*5)
 
         await interaction.response.send_message(
             f"You are now helping: {entry.username} - {"In-Person" if entry.in_person else "Online"} - {"Passoff - " if entry.is_passoff else ""}{entry.details}",
-            ephemeral=True
+            ephemeral=True,
+            delete_after=60*5
         )
 
     @discord.ui.button(label="Next Online", style=discord.ButtonStyle.blurple, custom_id="next_online", emoji="💻")
@@ -110,16 +111,17 @@ class TAView(discord.ui.View):
         entry: Optional[QueueEntry] = await interaction.client.queue.next(online_only=True)
 
         if not entry:
-            return await interaction.response.send_message("No online students available.", ephemeral=True)
+            return await interaction.response.send_message("No online students available.", ephemeral=True, delete_after=10)
 
         if not entry.is_passoff:
             increment_help(entry.user_id, entry.username)
 
-        await notify_tas(interaction, f"{interaction.user.display_name} is now helping {entry.username}")
+        await notify_tas(interaction, f"{interaction.user.display_name} is now helping {entry.username}",delete_after=60*5)
 
         await interaction.response.send_message(
             f"You are now helping: {entry.username} - {"In-Person" if entry.in_person else "Online"} - {"Passoff - " if entry.is_passoff else ""}{entry.details}",
-            ephemeral=True
+            ephemeral=True,
+            delete_after=60*5
         )
     
     @discord.ui.button(label="Next Passoff", style=discord.ButtonStyle.blurple, custom_id="next_passoff", emoji="✅")
@@ -127,14 +129,15 @@ class TAView(discord.ui.View):
         entry: Optional[QueueEntry] = await interaction.client.queue.next(passoff_only=True)
 
         if not entry:
-            return await interaction.response.send_message("No students awaiting passoff.", ephemeral=True)
+            return await interaction.response.send_message("No students awaiting passoff.", ephemeral=True, delete_after=10)
 
         await notify_tas(interaction, f"{interaction.user.display_name} is now helping {entry.username}")
 
 
         await interaction.response.send_message(
             f"You are now helping: {entry.username} - {"In-Person" if entry.in_person else "Online"} - {"Passoff - " if entry.is_passoff else ""}{entry.details}",
-            ephemeral=True
+            ephemeral=True,
+            delete_after=60*5
         )
 
     @discord.ui.button(label="Next Online Passoff", style=discord.ButtonStyle.blurple, custom_id="next_online_passoff", emoji="☑️")
@@ -142,17 +145,18 @@ class TAView(discord.ui.View):
         entry: Optional[QueueEntry] = await interaction.client.queue.next(passoff_only=True, online_only=True)
 
         if not entry:
-            return await interaction.response.send_message("No students awaiting online passoff.", ephemeral=True)
+            return await interaction.response.send_message("No students awaiting online passoff.", ephemeral=True, delete_after=10)
 
         await notify_tas(interaction, f"{interaction.user.display_name} is now helping {entry.username}")
 
 
         await interaction.response.send_message(
             f"You are now helping: {entry.username} - {"In-Person" if entry.in_person else "Online"} - {"Passoff - " if entry.is_passoff else ""}{entry.details}",
-            ephemeral=True
+            ephemeral=True,
+            delete_after=60*5
         )
 
-    @discord.ui.button(label="Student Info", style=discord.ButtonStyle.red, custom_id="student_info")
+    @discord.ui.button(label="Student Info", style=discord.ButtonStyle.secondary, custom_id="student_info", emoji="📝")
     async def student_info(self, interaction: discord.Interaction, button):
         info: tuple = get_student_info()
         builder: str = "```Student Info:\n"
@@ -172,6 +176,10 @@ class TAView(discord.ui.View):
     @discord.ui.button(label="Clear Queue", style=discord.ButtonStyle.danger, custom_id="clear_queue", emoji="💥")
     async def clear_queue(self, interaction: discord.Interaction, button):
         await interaction.response.send_modal(ClearConfirmModal())
+
+    @discord.ui.button(label="Remove Student", style=discord.ButtonStyle.danger, custom_id="remove_from_queue", emoji="🗑️")
+    async def remove_from_queue(self, interaction: discord.Interaction, button):
+        await interaction.response.send_modal(RemoveConfirmModal())
 
 def fixed_width(text: str, width: int) -> str:
     if len(text) > width:
